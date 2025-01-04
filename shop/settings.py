62c51421cb7304ec3d2b6312357fcaa29fd225b7
zhuk-zhenya <zhukzhenya77@gmail.com>
2025-01-04 15:47:04 +0300

    replacing the database, adding users

--- shop/settings.py
+++ shop/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'kvetka',
     'cart',
+    'users',
 ]
 
 MIDDLEWARE = [
@@ -80,8 +81,12 @@ WSGI_APPLICATION = 'shop.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'aikvetka',
+        'USER': 'aikvetka',
+        'PASSWORD': 'aikvetka',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
